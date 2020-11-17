95a59f6f744b802cfa576a1d2c7ae6c12a316dea
shashank <shashankshashu200@gail.com>
2020-11-18 00:33:53 +0530

    InitialCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Cards, Chart, CountryPicker, Footer} from './components';
+import styles from './App.module.css';
+import {fetchData} from './api';
+import CoronaImage from './images/image.png';
+class App extends React.Component{
+    state = {
+        data:{},
+        country:'',
+    }
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({data:fetchedData})
+    }
+    handleCountryChange = async(country) =>{
+        
+        const fetchedData = await fetchData(country);
+        this.setState({data:fetchedData,country:country});
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    }
+    render(){ 
+        const {data, country} = this.state;
+    return(
+        <div className = {styles.container}>
+            <img src={CoronaImage} className={styles.image} alt="Covid-19"/>
+            <Cards  data={this.state.data} /> 
+            <CountryPicker handleCountryChange={this.handleCountryChange}/>
+            <Chart data ={data} country={country}/>    
+            <Footer />        
+        </div>
+    )
+    }
 }
+
 
 export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Footer.module.css'
+
+function Footer(){
+    return(
+        <>  
+            <footer className={styles.footer}>
+                Shashank | 2020
+            </footer>
+        </>
+    )
+}
+
+export default Footer
